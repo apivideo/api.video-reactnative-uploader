d75143a6cbf01cd50bef5b1434d2afc81e6aa2e9
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2024-03-28 15:29:09 +0100

    feat(android): add API for progressive upload

--- android/src/main/java/video/api/reactnative/uploader/UploaderModule.kt
+++ android/src/main/java/video/api/reactnative/uploader/UploaderModule.kt
@@ -88,6 +88,56 @@ class UploaderModule(reactContext: ReactApplicationContext) :
     }
   }
 
+  // Progressive upload
+  @ReactMethod
+  override fun createProgressiveUploadSession(sessionId: String, videoId: String) {
+    uploaderModuleImpl.createUploadProgressiveSession(sessionId, videoId)
+  }
+
+  @ReactMethod
+  override fun createProgressiveUploadWithUploadTokenSession(
+    sessionId: String,
+    token: String,
+    videoId: String?
+  ) {
+    uploaderModuleImpl.createUploadWithUploadTokenProgressiveSession(sessionId, token, videoId)
+  }
+
+  @ReactMethod
+  override fun uploadPart(sessionId: String, filePath: String, promise: Promise) {
+    uploadPart(sessionId, filePath, false, promise)
+  }
+
+  @ReactMethod
+  override fun uploadLastPart(sessionId: String, filePath: String, promise: Promise) {
+    uploadPart(sessionId, filePath, true, promise)
+  }
+
+  @ReactMethod
+  override fun disposeProgressiveUploadSession(sessionId: String) {
+    uploaderModuleImpl.disposeProgressiveUploadSession(sessionId)
+  }
+
+  private fun uploadPart(
+    sessionId: String,
+    filePath: String,
+    isLastPart: Boolean,
+    promise: Promise
+  ) {
+    try {
+      uploaderModuleImpl.uploadPart(sessionId, filePath, isLastPart, { _ ->
+      }, { video ->
+        promise.resolve(video)
+      }, {
+        promise.reject(CancellationException("Upload was cancelled"))
+      }, { e ->
+        promise.reject(e)
+      })
+    } catch (e: Exception) {
+      promise.reject(e)
+    }
+  }
+
   companion object {
     const val NAME = "ApiVideoUploader"
 
